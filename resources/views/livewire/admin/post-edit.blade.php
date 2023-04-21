b6ff83c57f85269d5b8f882a1ad88dd01abf1a3d
Sourov Biswas <sourovbuzz@gmail.com>
2023-04-21 12:43:56 +0600

    elfinder

--- resources/views/livewire/admin/post-edit.blade.php
+++ resources/views/livewire/admin/post-edit.blade.php
@@ -107,8 +107,54 @@
                     editor.on('change', function(e) {
                         @this.set('post.content', editor.getContent());
                     });
-                }
+                },
+                file_picker_callback: elFinderBrowser,
+                image_dimensions: false,
             });
+
+            function elFinderBrowser(callback, value, meta) {
+                tinymce.activeEditor.windowManager.openUrl({
+                    title: 'File Manager',
+                    url: "{{ route('elfinder.tinymce5') }}",
+                    /**
+                     * On message will be triggered by the child window
+                     * 
+                     * @param dialogApi
+                     * @param details
+                     * @see https://www.tiny.cloud/docs/ui-components/urldialog/#configurationoptions
+                     */
+                    onMessage: function(dialogApi, details) {
+                        if (details.mceAction === 'fileSelected') {
+                            const file = details.data.file;
+
+                            // Make file info
+                            const info = file.name;
+
+                            // Provide file and text for the link dialog
+                            if (meta.filetype === 'file') {
+                                callback(file.url, {
+                                    text: info,
+                                    title: info
+                                });
+                            }
+
+                            // Provide image and alt text for the image dialog
+                            if (meta.filetype === 'image') {
+                                callback(file.url, {
+                                    alt: info
+                                });
+                            }
+
+                            // Provide alternative source and posted for the media dialog
+                            if (meta.filetype === 'media') {
+                                callback(file.url);
+                            }
+
+                            dialogApi.close();
+                        }
+                    }
+                });
+            }
         </script>
     @endpush
 </div>
