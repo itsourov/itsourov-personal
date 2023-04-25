95a8198c86ce13bbe750a12722ce1f03fde18a52
Sourov Biswas <sourovbuzz@gmail.com>
2023-04-25 14:13:53 +0600

    sidebar submenu menu dotted

--- resources/views/components/admin/sidebar-sub-menu-item.blade.php
+++ resources/views/components/admin/sidebar-sub-menu-item.blade.php
@@ -6,7 +6,7 @@
     } else {
         $classes = '';
     }
-    $classes = $classes . ' flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700';
+    $classes = $classes . ' flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700 border border-dashed dark:border-gray-700';
     
 @endphp
 
