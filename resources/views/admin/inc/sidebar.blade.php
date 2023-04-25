759e3a0dc320e107e665faea8cf6f9ae0dbb21d0
Sourov Biswas <sourovbuzz@gmail.com>
2023-04-25 12:30:39 +0600

    admin sidebar menu icon

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -30,17 +30,11 @@
                     <x-admin.sidebar-menu-item :active="request()->routeIs('admin.posts*')" :dropdown="true">
 
                         <x-slot name="icon">
-                            <svg class="w-5 h-5" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                stroke="currentColor" stroke-width="1.5">
-                                <rect x="8" y="8" width="12" height="12" />
-                                <rect x="26" y="8" width="12" height="12" />
-                                <rect x="26" y="44" width="12" height="12" />
-                                <rect x="44" y="8" width="12" height="12" />
-                                <rect x="8" y="26" width="12" height="12" />
-                                <rect x="26" y="26" width="12" height="12" />
-                                <rect x="44" y="26" width="12" height="12" />
-                                <rect x="8" y="44" width="12" height="12" />
-                                <rect x="44" y="44" width="12" height="12" />
+                            <svg class="h-5 w-5" fill="none" stroke="currentColor" stroke-width="1.5"
+                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z">
+                                </path>
                             </svg>
                         </x-slot>
                         <x-slot name="title">
@@ -64,17 +58,11 @@
                     <x-admin.sidebar-menu-item :active="request()->routeIs('admin.products*')" :dropdown="true">
 
                         <x-slot name="icon">
-                            <svg class="w-5 h-5" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                stroke="currentColor" stroke-width="1.5">
-                                <rect x="8" y="8" width="12" height="12" />
-                                <rect x="26" y="8" width="12" height="12" />
-                                <rect x="26" y="44" width="12" height="12" />
-                                <rect x="44" y="8" width="12" height="12" />
-                                <rect x="8" y="26" width="12" height="12" />
-                                <rect x="26" y="26" width="12" height="12" />
-                                <rect x="44" y="26" width="12" height="12" />
-                                <rect x="8" y="44" width="12" height="12" />
-                                <rect x="44" y="44" width="12" height="12" />
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="1.5"
+                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z">
+                                </path>
                             </svg>
                         </x-slot>
                         <x-slot name="title">
@@ -99,8 +87,8 @@
                     <x-admin.sidebar-menu-item :active="request()->routeIs('admin.categories.index')" :href="route('admin.categories.index')">
 
                         <x-slot name="icon">
-                            <svg class="w-5 h-5" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" stroke="currentColor" stroke-width="1.5">
+                            <svg class="w-5 h-5" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                stroke="currentColor" stroke-width="1.5">
                                 <rect x="8" y="8" width="12" height="12" />
                                 <rect x="26" y="8" width="12" height="12" />
                                 <rect x="26" y="44" width="12" height="12" />
@@ -124,7 +112,12 @@
                     <x-admin.sidebar-menu-item :active="request()->routeIs('admin.orders*')" :dropdown="true">
 
                         <x-slot name="icon">
-                            {{-- <x-ri-article-line /> --}}
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="1.5"
+                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z">
+                                </path>
+                            </svg>
                         </x-slot>
                         <x-slot name="title">
                             {{ __('Orders') }}
