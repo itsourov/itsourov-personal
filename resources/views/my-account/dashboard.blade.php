10f0851c7d1b026d4108b84f18853f80c4f0dbae
Sourov Biswas <sourovbuzz@gmail.com>
2023-04-27 02:51:08 +0600

    my account icons

--- resources/views/my-account/dashboard.blade.php
+++ resources/views/my-account/dashboard.blade.php
@@ -6,15 +6,20 @@
             addresses, and edit your password and account details.</p>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
 
-            <x-my-account.dashbord-item :href="route('my-account.orders')" :imageLink="asset('images/flaticon/orders.png')">
-                Orders
+            <x-my-account.dashbord-item :href="route('my-account.orders')" title="{{ __('Orders') }}">
+
+                <x-svg.clipboard-document class="w-12 h-12" stroke-width="0.5" />
             </x-my-account.dashbord-item>
-            <x-my-account.dashbord-item :href="route('my-account.downloads')" :imageLink="asset('images/flaticon/download.png')">
-                Downloads
+            <x-my-account.dashbord-item :href="route('my-account.downloads')" title="{{ __('Downloads') }}">
+
+                <x-svg.arrow-down-tray class="w-12 h-12" stroke-width="0.5" />
             </x-my-account.dashbord-item>
-            <x-my-account.dashbord-item :href="route('my-account.profile.edit')" :imageLink="asset('images/flaticon/profile.png')">
-                Profile
+            <x-my-account.dashbord-item :href="route('my-account.profile.edit')" title="{{ __('Profile') }}">
+
+                <x-svg.user-circle class="w-12 h-12" stroke-width="0.5" />
+
             </x-my-account.dashbord-item>
+
 
         </div>
     </div>
