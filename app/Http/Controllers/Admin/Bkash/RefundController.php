0e1227bc38369809575e80297e4fd670fdde3d34
Sourov Biswas <sourovbuzz@gmail.com>
2023-04-27 19:11:00 +0600

    bkash refund logic changed.

--- app/Http/Controllers/Admin/Bkash/RefundController.php
+++ app/Http/Controllers/Admin/Bkash/RefundController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin\Bkash;
 
 use App\Enums\OrderStatus;
+use App\Http\Helpers\Bkash;
 use App\Enums\PaymentStatus;
 use Illuminate\Http\Request;
+use App\Models\BkashTransaction;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
@@ -13,6 +15,7 @@ class RefundController extends Controller
     public function refundPaymeent(Request $request, BkashTransaction $bkashTransaction)
     {
 
+        // new start
         $validated = $request->validate([
             'reason' => 'max:255',
             'cancel_order' => 'boolean',
@@ -20,9 +23,34 @@ class RefundController extends Controller
 
         ]);
 
+        DB::beginTransaction();
 
+        if ($bkashTransaction->bkash_transactionable_type == Order::class) {
 
+            $order = $bkashTransaction->bkashTransactionable;
+            if (!$order) {
+                return back()->with('message', 'The order was not found for this transaction');
+            }
 
+            if ($request->has('cancel_order')) {
+                auth()->user()->purchasedItems()->detach($order->products->pluck('id'));
+                $order->update([
+                    'order_status' => OrderStatus::Cancled,
+                    'payment_status' => PaymentStatus::Refunded,
+                ]);
+
+                $order->activities()->create([
+                    'action_by' => 'admin',
+                    'content' => 'Bkash payment was refunded, Order Status: ' . OrderStatus::Cancled . ', and Payment status: ' . PaymentStatus::Refunded . ' .',
+                ]);
+            } else {
+                $order->activities()->create([
+                    'action_by' => 'admin',
+                    'content' => 'Bkash payment was refunded but the order status was unchanged, Payment status: ' . PaymentStatus::Refunded . ' .',
+                ]);
+            }
+
+        }
         $refundResponse = Bkash::refundPayment(
         paymentID: $bkashTransaction->paymentID,
         trxID: $bkashTransaction->trxID,
@@ -30,6 +58,8 @@ class RefundController extends Controller
         reason: $validated['reason'],
         sku: $bkashTransaction->bkash_transactionable_type . "-" . $bkashTransaction->bkash_transactionable_id
         );
+
+
 
         if ($refundResponse->status() == 401) {
             Bkash::refresh_token();
@@ -44,32 +74,11 @@ class RefundController extends Controller
         }
 
 
+
         if ($refundResponse->json('transactionStatus') == 'Completed') {
-            DB::beginTransaction();
 
             $bkashTransaction->update(array_merge($refundResponse->json(), ['transactionStatus' => 'Refunded']));
 
-
-
-            if ($bkashTransaction->bkash_transactionable_type == Order::class && $request->has('cancel_order')) {
-
-                $order = $bkashTransaction->bkashTransactionable;
-                if (!$order) {
-                    return back()->with('message', 'The order was not found for this transaction');
-                }
-
-                auth()->user()->purchasedItems()->detach($order->products->pluck('id'));
-                $order->update([
-                    'order_status' => OrderStatus::Cancled,
-                    'payment_status' => PaymentStatus::Refunded,
-                ]);
-
-            }
-
-            $order->activities()->create([
-                'action_by' => 'admin',
-                'content' => 'Bkash payment was refunded, Order Status: ' . OrderStatus::processing . ', and Payment status: ' . PaymentStatus::Paid . ' .',
-            ]);
             DB::commit();
             return back()->with('message', "Refunded to customer complite");
         } else {
