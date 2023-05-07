5d404acc7a7065e3c950757fb9cf4bd9fab7a2ca
itsourov <sourovbuzz@gmail.com>
2023-05-07 15:12:56 +0600

    nothing

--- database/migrations/2023_04_20_043038_create_comments_table.php
+++ database/migrations/2023_04_20_043038_create_comments_table.php
@@ -14,8 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->integer('parent_id')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->integer('commentable_id')->unsigned();
-            $table->string('commentable_type');
+            $table->morphs('commentable');
             $table->text('comment');
             $table->enum('rating', [1, 2, 3, 4, 5])->default(5);
             $table->softDeletes();
