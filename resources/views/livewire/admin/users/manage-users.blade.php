09afab01f39c4d64aa83fd07e33275084ec0401e
itsourov <sourovbuzz@gmail.com>
2023-05-14 08:32:25 +0600

    user status

--- resources/views/livewire/admin/users/manage-users.blade.php
+++ resources/views/livewire/admin/users/manage-users.blade.php
@@ -43,9 +43,15 @@
                                     </td>
 
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <span
-                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                            Active </span>
+                                        @if ($user->email_verified_at)
+                                            <span
+                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                Verified </span>
+                                        @else
+                                            <span
+                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                                Un-verified </span>
+                                        @endif
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $user->role }}
                                     </td>
